2d22c39405f80f47bd2efc5fd2b87a772b1d8a0a
Noelani West <noelani.rose14@gmail.com>
2022-10-27 10:50:14 -0500

    create fetchPizzas function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+// IMPORT COMPONENTS 
+
+
+
 
 function App() {
+  const dispatch = useDispatch();
+
+// On page load, fetch Pizzas
+  useEffect(() => {
+    fetchPizzas();
+  }, [])
+
+
+// AXIOS REQUEST (GET) FOR ALL PIZZAS
+const fetchPizzas = () => {
+  console.log('in GET fetch pizzas function')
+  axios({
+    method: 'GET', 
+    url: '/api/pizza'
+  })
+  .then((res) => {
+    dispatch ({
+      type: 'DISPALY_PIZZAS',
+      payload: res.data
+    })
+  })
+    .catch((err) => {
+      console.log ('error GETting display pizzas', err)
+  })
+}
+
+
+// AXIOS REGUEST (GET) FOR ALL ORDERS 
+
+
+
+
+
+// AXIOS REQUEST (POST) FOR CUSTOMER ORDERS 
+
+
 
   return (
     <div className='App'>
